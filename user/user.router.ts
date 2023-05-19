005e1e97f00861b827a95499638a65b030ff0114
andrew28092002 <andreiastanaev2002@gmail.com>
2023-05-19 15:29:09 +0500

    add middleware to user controller

--- user/user.router.ts
+++ user/user.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import userController from "./user.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 
 const userRouter = Router();
 
-userRouter.get("/getAll", userController.findAll);
+userRouter.get("/getAll", authMiddleware, userController.findAll);
 
 export { userRouter };
